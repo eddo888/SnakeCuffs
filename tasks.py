7abc53b49a766a9a88e8913c5f3b743f1057d15d
David Edson <eddo888@tpg.com.au>
2020-07-31 16:14:06 +1000

    pok

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+print("usage: invoke all")
+
 """ Task definitions for invoke command line utility for python bindings
     overview article. """
 import os, sys, re, cffi, invoke, pathlib
